d2795a9e089665d62ba420636e7787e1c093b4e9
Supriya Limbole <127923605+BoonDocks-sl@users.noreply.github.com>
2023-06-29 11:06:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const players = ["John", "Bob", "Alice", "Poppy"];
+// Get the <p> element by its id
+const timerElement = document.getElementById('timer');
 
-const person = {
-  name: "John Doe",
-  age: 80,
-};
+// Function to update the timer
+function updateTimer() {
+  // Get the current date and time
+  const currentDate = new Date();
 
-// Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+  // Extract the date, month, year, hours, minutes, and seconds from the current date
+  const day = currentDate.getDate();
+  const month = currentDate.getMonth() + 1; // Months are zero-based, so we add 1
+  const year = currentDate.getFullYear();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+  // Format the date and time as a string
+  const timerText = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+
+  // Update the <p> element with the timer text
+  timerElement.textContent = timerText;
+}
+
+// Call the updateTimer function initially to set the initial timer value
+updateTimer();
+
+// Update the timer every second (1000 milliseconds)
+setInterval(updateTimer, 1000);
